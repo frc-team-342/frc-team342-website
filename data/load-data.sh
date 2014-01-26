50d83d872dd1cef4ab5cfe6f5210b7c3c97b6947
Adam Brightwell <abrightwell@lce.com>
2014-01-26 13:06:23 -0500

    added data load script for development.

---
+++ data/load-data.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+MONGO_CMD="/usr/bin/mongo"
+DB="team342-site"
+DATA_FILES=("awards.json" "mentors.json" "schedule.json" "students.json")
+
+for data_file in "${DATA_FILES[@]}"
+do
+	echo "Loading data: ${data_file}"
+	"${MONGO_CMD}" "${DB}" < "${data_file}"
+done
