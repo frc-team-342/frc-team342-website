43af7139c73560a0fe847746c706dbc79353582c
Adam Brightwell <abrightwell@lce.com>
2014-01-26 12:35:32 -0500

    added students dao to subteam controller

--- routes/subteam_controller.js
+++ routes/subteam_controller.js
@@ -1,10 +1,11 @@
 var async = require('async');
 var MentorDAO = require('../dao/mentor').MentorDAO;
-// var StudentDAO = require('../dao/student');
+var StudentDAO = require('../dao/student-dao').StudentDAO;
 
 function SubteamController(db) {
 
 	var mentorDao = new MentorDAO(db);
+	var studentDao = new StudentDAO(db);
 
 	this.index = function(req, res) {
 		res.format({
@@ -19,7 +20,8 @@ function SubteamController(db) {
 
 		async.parallel({
 			mentors: function(callback) {
-				mentorDao.findBySubteam(subteam, function(err, mentors){
+				var options = {fields: {'subteam': 0}};
+				mentorDao.findBySubteam(subteam, options, function(err, mentors){
 					if (err) {
 						callback(err, null);
 					} else {
@@ -28,9 +30,18 @@ function SubteamController(db) {
 				});
 			},
 			students: function(callback) {
-				callback(null, {});
+				var options = {sort: { 'grade': -1 }, fields: {'subteam': 0}};
+
+				studentDao.findBySubteam(subteam, options, function(err, students) {
+					if (err) {
+						callback(err, null);
+					} else {
+						callback(err, students);
+					}
+				});
 			}
 		}, function(err, subteam) {
+			subteam.name = req.params.subteam;
 			res.format({
 				html: function() {
 					res.render('subteams/show', subteam);
